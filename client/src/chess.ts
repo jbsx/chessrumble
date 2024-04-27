0282ac8fd9ae7c0ac683f158767dcc296923c6d9
jbsx <jaishsingh@hotmail.com>
2024-04-27 13:36:58 +0530

    ws

--- client/src/chess.ts
+++ client/src/chess.ts
@@ -1,17 +1,14 @@
 import axios from "axios";
-import { writable } from "svelte/store";
+import {writable} from "svelte/store";
 
-export type Type = "k" | "q" | "r" | "b" | "n" | "p";
-export type Team = "w" | "b";
+export type Type = "k"|"q"|"r"|"b"|"n"|"p";
+export type Team = "w"|"b";
 
 export let boardTeam = writable("w" as Team);
 
 export let board = writable(
-    Array(8)
-        .fill([])
-        .map(() => {
-            return new Array(8).fill(null);
-        }) as Array<Array<Piece | null>>,
+    Array(8).fill([]).map(() => { return new Array(8).fill(null);}) as
+        Array<Array<Piece|null>>,
 );
 
 export class Piece {
@@ -26,7 +23,7 @@ export class Piece {
 
 export default class Chess {
     player: Team;
-    _conn: WebSocket | null;
+    _conn: WebSocket|null;
     fen: string;
 
     constructor(id: string) {
@@ -38,37 +35,36 @@ export default class Chess {
 
         axios
             .get(`http://localhost:3000/game/join/${id}`, {
-                withCredentials: true,
+                withCredentials : true,
             })
             .then(() => {
                 this._conn = new WebSocket(`ws://localhost:3000/game/${id}`);
 
-                this._conn.addEventListener("open", () => {
-                    console.log("WebSocket connection established");
-                });
+                this._conn.addEventListener(
+                    "open",
+                    () => { console.log("WebSocket connection established"); });
 
                 this._conn.addEventListener("message", (msg) => {
                     const data = msg.data.split(" ");
                     switch (data[0]) {
-                        case "FEN":
-                            this.fen = data.slice(1, data.length).join(" ");
-                            console.log(this.fen);
-                            this.updateBoard();
-                            break;
-                        case "TEAM":
-                            console.log(data[1]);
-                            this.player = data[1] as Team;
-                            boardTeam.set(this.player);
-                            this.updateBoard();
-                            break;
-                        default:
-                            console.log("default");
+                    case "FEN":
+                        this.fen = data.slice(1, data.length).join(" ");
+                        console.log(this.fen);
+                        this.updateBoard();
+                        break;
+                    case "TEAM":
+                        console.log(data[1]);
+                        this.player = data[1] as Team;
+                        boardTeam.set(this.player);
+                        this.updateBoard();
+                        break;
+                    default:
+                        console.log("default");
                     }
                 });
 
-                this._conn.addEventListener("close", () => {
-                    console.log("connnection closed");
-                });
+                this._conn.addEventListener(
+                    "close", () => { console.log("connnection closed"); });
             });
 
         this.updateBoard();
@@ -76,20 +72,30 @@ export default class Chess {
 
     updateBoard() {
         board.update(() => {
-            const temp = Array(8)
-                .fill([])
-                .map(() => {
-                    return Array(8).fill(null);
-                }) as Array<Array<Piece | null>>;
+            const temp =
+                Array(8).fill([]).map(() => { return Array(8).fill(null); }) as
+                Array<Array<Piece|null>>;
 
-            //Loop through FEN and populate board
+            // Loop through FEN and populate board
             if (this.player == "w") {
-                this.fen
-                    .split(" ")[0]
-                    .split("/")
-                    .forEach((rank, x) => {
+                this.fen.split(" ")[0].split("/").forEach((rank, x) => {
+                    let offset = 0;
+                    rank.split("").forEach((n, y) => {
+                        if (parseInt(n)) {
+                            offset += parseInt(n) - 1;
+                        } else {
+                            temp[x][y + offset] = new Piece(
+                                n == n.toUpperCase() ? "w" : "b",
+                                n as Type,
+                            );
+                        }
+                    });
+                });
+            } else {
+                this.fen.split(" ")[0].split("/").reverse().forEach(
+                    (rank, x) => {
                         let offset = 0;
-                        rank.split("").forEach((n, y) => {
+                        rank.split("").reverse().forEach((n, y) => {
                             if (parseInt(n)) {
                                 offset += parseInt(n) - 1;
                             } else {
@@ -99,26 +105,6 @@ export default class Chess {
                                 );
                             }
                         });
-                    });
-            } else {
-                this.fen
-                    .split(" ")[0]
-                    .split("/")
-                    .reverse()
-                    .forEach((rank, x) => {
-                        let offset = 0;
-                        rank.split("")
-                            .reverse()
-                            .forEach((n, y) => {
-                                if (parseInt(n)) {
-                                    offset += parseInt(n) - 1;
-                                } else {
-                                    temp[x][y + offset] = new Piece(
-                                        n == n.toUpperCase() ? "w" : "b",
-                                        n as Type,
-                                    );
-                                }
-                            });
                     });
             }
 
